125384a2daedad839984cf92889a70f76c693038
Will Zegers <will.zegers@braincorp.com>
2020-12-02 19:16:11 -0800

    Add some utility functions and consts

---
+++ include/raytracer/util.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <limits>
+#include <random>
+
+const double kInfinity = std::numeric_limits<double>::infinity();
+
+inline double RandomDouble() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double RandomDouble(double min, double max) {
+    static std::uniform_real_distribution<double> distribution(min, max);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
